e4e07dce85e48346653bcb4cb3f7befa24714dcf
Andrii Kopp <kopp93@gmail.com>
2020-12-27 11:11:06 +0200

    calculate all quality measures

--- quality/assessment.js
+++ quality/assessment.js
@@ -8,8 +8,12 @@ function assessQuality(process, measures, uid, raw) {
     var discrete = measurement.calculateDiscreteCriteria(measures);
     var continuous = measurement.calculateContinuousCriteria(measures);
 
-    var discreteQuality = Math.min(discrete.R1, discrete.R2, discrete.R3, discrete.R4, discrete.R5);
-    var continuousQuality = 0.21 * continuous.R1 + 0.19 * continuous.R2 + 0.16 * continuous.R3 + 0.28 * continuous.R4 + 0.16 * continuous.R5;
+    var discreteQualityWSM = 0.21 * discrete.R1 + 0.19 * discrete.R2 + 0.16 * discrete.R3 + 0.28 * discrete.R4 + 0.16 * discrete.R5;
+    var discreteQualityMIN = Math.min(discrete.R1, discrete.R2, discrete.R3, discrete.R4, discrete.R5);
+
+    var continuousQualityWSM = 0.21 * continuous.R1 + 0.19 * continuous.R2 + 0.16 * continuous.R3 + 0.28 * continuous.R4 + 0.16 * continuous.R5;
+    var continuousQualityMIN = Math.min(continuous.R1, continuous.R2, continuous.R3, continuous.R4, continuous.R5);
+
 
     var report = {
         process: process,
@@ -19,8 +23,14 @@ function assessQuality(process, measures, uid, raw) {
             continuous: continuous
         },
         quality: {
-            discrete: discreteQuality,
-            continuous: continuousQuality.toFixed(2)
+            discrete: {
+                wsm: discreteQualityWSM.toFixed(2),
+                min: discreteQualityMIN
+            },
+            continuous: {
+                wsm: continuousQualityWSM.toFixed(2),
+                min: continuousQualityMIN.toFixed(2)
+            }
         },
         timestamp: new Date().toLocaleString(),
         uid: uid,
