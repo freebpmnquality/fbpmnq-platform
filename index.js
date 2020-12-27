314c994f9338b698909a62df0eda7398fb4e4387
Andrii Kopp <kopp93@gmail.com>
2020-12-27 11:43:57 +0200

    refactor entry point

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ var registration = require('./user/registration');
 var assessment = require('./quality/assessment');
 var reporting = require("./quality/reporting");
 
-app.use(express.static(__dirname + "/public"));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 app.post("/api/user/authorization", jsonParser, function(req, res) {
     var login = req.body.login;
