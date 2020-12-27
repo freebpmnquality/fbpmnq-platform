6fed2d2a2de7ba92083b31402cb7887f48249f85
Andrii Kopp <kopp93@gmail.com>
2020-12-27 11:22:45 +0200

    complete quality measurement

--- quality/assessment.js
+++ quality/assessment.js
@@ -14,7 +14,6 @@ function assessQuality(process, measures, uid, raw) {
     var continuousQualityWSM = 0.21 * continuous.R1 + 0.19 * continuous.R2 + 0.16 * continuous.R3 + 0.28 * continuous.R4 + 0.16 * continuous.R5;
     var continuousQualityMIN = Math.min(continuous.R1, continuous.R2, continuous.R3, continuous.R4, continuous.R5);
 
-
     var report = {
         process: process,
         measures: {
@@ -24,12 +23,16 @@ function assessQuality(process, measures, uid, raw) {
         },
         quality: {
             discrete: {
-                wsm: discreteQualityWSM.toFixed(2),
-                min: discreteQualityMIN
+                wsm: discreteQualityWSM,
+                min: discreteQualityMIN,
+                wsml: getLinguisticQuality(discreteQualityWSM),
+                minl: getLinguisticQuality(discreteQualityMIN)
             },
             continuous: {
-                wsm: continuousQualityWSM.toFixed(2),
-                min: continuousQualityMIN.toFixed(2)
+                wsm: continuousQualityWSM,
+                min: continuousQualityMIN,
+                wsml: getLinguisticQuality(continuousQualityWSM),
+                minl: getLinguisticQuality(continuousQualityMIN)
             }
         },
         timestamp: new Date().toLocaleString(),
@@ -40,6 +43,30 @@ function assessQuality(process, measures, uid, raw) {
     reporting.saveReport(report);
 
     return report;
+}
+
+function getLinguisticQuality(quality) {
+    if (quality >= 0.80 && quality <= 1.00) {
+        return "Good";
+    }
+
+    if (quality >= 0.64 && quality < 0.80) {
+        return "Well";
+    }
+
+    if (quality >= 0.37 && quality < 0.64) {
+        return "Satisfied";
+    }
+
+    if (quality >= 0.20 && quality < 0.37) {
+        return "Poor";
+    }
+
+    if (quality >= 0 && quality < 0.20) {
+        return "Bad";
+    }
+
+    return "Indefinable";
 }
 
 module.exports.assessQuality = assessQuality;
